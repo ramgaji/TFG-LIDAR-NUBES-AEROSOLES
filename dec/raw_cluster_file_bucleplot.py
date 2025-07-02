6cbbcad8710a6bca6e02d8e9ff70f2cce666219d
ramgaji <ramgaji@gmail.com>
2025-07-02 02:04:45 +0200

    Update raw_cluster_file_bucleplot.py

--- dec/raw_cluster_file_bucleplot.py
+++ dec/raw_cluster_file_bucleplot.py
@@ -19,7 +19,7 @@ from skimage.transform import resize
 import xarray as xr
 
 #   ruta de datos
-data_path = '../data'
+data_path = './data'
 
 #  Obtener la lista de archivos en la carpeta data (adaptado a granada)
 files = sorted([f for f in os.listdir(f"{data_path}") if f.endswith(".nc")])
